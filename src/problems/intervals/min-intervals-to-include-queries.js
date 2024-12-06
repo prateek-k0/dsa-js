5feff06d1e86bad3df32ebbc40563ca537c76803
Prateek Katkar <parx1601@gmail.com>
2024-12-06 16:51:21 +0530

    min-intervals-to-update-query added

--- src/problems/intervals/min-intervals-to-include-queries.js
+++ src/problems/intervals/min-intervals-to-include-queries.js
@@ -3,86 +3,113 @@
 import { MinPriorityQueue } from "datastructures-js";
 
 /*
-    Plot entry, exit and queries on the lines
-
-    TODO: make faster
+    We first sort both the intervals and queries array (only a copy of queries array)
+    Then for each query, 
+        - we first discard all intervals from intervals array that end before the current query
+        it is guaranteed that these intervals will NOT contain any query later, since both
+        queries and intervals are sorted
+        - then, we put all intervals into the PQ that start before or 
+        at the same point as the current query
+        - then we remove all the intervals from PQ that are outside (ending before current query).
+        - then, what we have in the PQ at the front is the smallest interval that contains the
+        current query, since PQ uses the interval size as the priority.
+        - if its empty, its -1
 */
 
-function getSize([start, end]) {
-    return end - start + 1;
-} 
-
+// slower version - due to using shift operation on arrays
 export function minIntervalToIncludeQuery(intervals, queries) {
-    let res = new Array(queries.length).fill(-1);
-    const lines = {};
-    // plot lines with attribute that is able to differentiate if a line starts or ends
-    for(const [start, end] of intervals) {
-        lines[start] = (lines[start] ?? []).concat({ key: 'add', val: [start, end] }); // line starts
-        lines[end] = (lines[end] ?? []).concat({ key: 'remove', val: [start, end] });    // line ends
-    }
-    // plot queries
-    for(let i = 0; i < queries.length; i++) {
-        lines[queries[i]] = (lines[queries[i]] ?? []).concat({ key: 'queries', val: i }); // store index so that we can store the results accordingly
-    }
-    console.log(lines);
-    const sizes = [];   // to store the sizes
-    // traverse all the lines
-    for(const valueList of Object.values(lines)) {
-        // all values stored in lines must be sorted as follows: add (entry of line), queries, remove (exit the line)
-        valueList.sort((a, b) => a.key.localeCompare(b.key));
-        for(const value of valueList) {
-            if(value.key === 'add') {
-                sizes.push(getSize(value.val));
-            } else if(value.key === 'remove') {
-                sizes.splice(sizes.indexOf(getSize(value.val)), 1);
-            } else {
-                if(sizes.length > 0) res[value.val] = Math.min(...sizes);
-            }
+    // sort intervals according to their start times for efficient traversals
+    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
+    // PQ to store intervals according to sizes
+    const minPQ = new MinPriorityQueue(([start, end]) => end - start + 1);  // size of the query = end - start + 1
+    // since we are changing the order due to sorting of queries, store them in a map for retrieval
+    const queryRes = {}
+    // sort all queries before processing
+    const queriesSorted = [...queries].sort((a, b) => a - b);
+    // process queries
+    for (const q of queriesSorted) {
+        // remove those intervals from the queue which are before the current query
+        // it is guranteed that these queries removed will not be used by later elements,
+        // since both queries and intervals are sorted
+        // intervals[0][1] < q implies that the interval at the front liest before current query
+        while (intervals.length > 0 && intervals[0][1] < q) {
+            intervals.shift();
+        }
+        // now we have intervals that may contain the query
+        // if the intervals at the front contain the query, put them in PQ
+        while (intervals.length > 0 && (intervals[0][0] <= q && q <= intervals[0][1])) {
+            const firstInterval = intervals.shift();
+            // only put those intervals in the PQ that contain the query
+            minPQ.enqueue(firstInterval);
+        }
+        // remove intervals from PQ that don't contain the query
+        while (minPQ.isEmpty() === false && minPQ.front()[1] < q) {
+            minPQ.dequeue();
+        }
+        // now, since we have intervals in PQ that contain the query, get its top
+        // that means, if PQ is empty, query cannot be contained by given intervals
+        if (minPQ.isEmpty() === false) {
+            const [frontStart, frontEnd] = minPQ.front();
+            queryRes[q] = frontEnd - frontStart + 1;
+        } else {    // query cant be contained
+            queryRes[q] = -1;
         }
     }
-    return res;
+
+    return queries.map((q) => queryRes[q]);
 }
 
 
 
-// faster version
-var minInterval = function(intervals, queries) {
-	//sort intervals and queries so we can process them in order
-    intervals = intervals.sort((a,b) => a[0]-b[0])
-	// careful here when sorting queries we must make a shallow copy so we dont sort the original array
-    let sortedQueries = [...queries].sort((a,b) => a-b)
-    let minInterval = {}
-	// use min heap to keep track of the smallest interval
-    let heap = new MinPriorityQueue({priority: (a) => a[1] - a[0] + 1})
-	// pointer tracks our position in intervals,
-  // since queries are sorted, we dont need to backtrack to previous intervals for each query, 
-  // a single pointer is enougn
-    let pointer = 0
-	
-    for (const query of sortedQueries) {
-		// if we have already seen the query value before we know the minimum interval
-		if (minInterval[query]) continue
-		// advancing our pointer until we find a new interval that contains our query
-        while (pointer < intervals.length && intervals[pointer][1] < query) {
-            pointer++
+// faster version - instead of having to remove intervals from intervals array
+//  that are before current query, we simply maintain a pointer and increment it
+// that behaves like intervals[pointer] === intervals[0];
+var minInterval = function (intervals, queries) {
+    // sort intervals according to their start times for efficient traversals
+    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
+    // PQ to store intervals according to sizes
+    const minPQ = new MinPriorityQueue(([start, end]) => end - start + 1);  // size of the query = end - start + 1
+    // since we are changing the order due to sorting of queries, store them in a map for retrieval
+    const queryRes = {}
+    // sort all queries before processing
+    const queriesSorted = [...queries].sort((a, b) => a - b);
+    // maintain a pointer to discard intervals before current query
+    let pointer = 0;
+    // process queries
+    for (const q of queriesSorted) {
+        // if already computed
+        if (queryRes[q]) continue;
+        // remove those intervals from the queue which are before the current query
+        // it is guranteed that these queries removed will not be used by later elements,
+        // since both queries and intervals are sorted
+        // instead of intervals.shift(), which is expensive, we can simply increment the pointer
+        while (pointer < intervals.length && intervals[pointer][1] < q) {
+            pointer++;
         }
-		// adding new intervals that arent in our heap that our query fits inside
-        while (pointer < intervals.length && intervals[pointer][0] <= query) {
-            heap.enqueue(intervals[pointer])
-            pointer++
+        // now we have intervals that may contain the query
+        // if the intervals at the front contain the query, put them in PQ
+        // while(pointer < intervals.length && (intervals[pointer][0] <= q && q <= intervals[pointer][1])) {
+        // only check if start is before or equal to current query, no need to check end
+        while (pointer < intervals.length && (intervals[pointer][0] <= q && q <= intervals[pointer][1])) {
+            const firstInterval = intervals[pointer];
+            // only put those intervals in the PQ that contain the query
+            minPQ.enqueue(firstInterval);
+            // increment pointer, since we've added intervals[pointer] to PQ
+            pointer++;
         }
-		// removing the top interval until we run out of elements or we find an interval that our query fits inside
-        while (heap.size() && heap.front().element[1] < query) {
-            heap.dequeue()
+        // remove intervals from PQ that don't contain the query
+        while (minPQ.isEmpty() === false && minPQ.front()[1] < q) {
+            minPQ.dequeue();
         }
-		// keep track of each query value 
-        minInterval[query] = (heap.size())? heap.front().priority: -1   
+        // now, since we have intervals in PQ that contain the query, get its top
+        // that means, if PQ is empty, query cannot be contained by given intervals
+        if (minPQ.isEmpty() === false) {
+            const [frontStart, frontEnd] = minPQ.front();
+            queryRes[q] = frontEnd - frontStart + 1;
+        } else {    // query cant be contained
+            queryRes[q] = -1;
+        }
     }
-    let ans = []
-	// since we sorted the queries earlier to get the proper order 
-	//we must go back through our queries and look up their value in the stored hash
-    for (let i = 0; i < queries.length; i++) {
-        ans[i] = minInterval[queries[i]]
-    }
-    return ans
+
+    return queries.map((q) => queryRes[q]);
 };
